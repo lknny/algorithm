ae878f9179981e2610b1114f24f02ce54825eab2
Kun Liu <lknny@163.com>
2018-04-25 15:10:36 +0800

    update

---
+++ src/main/java/lk/二分查找.java
@@ -0,0 +1,48 @@
+package lk;
+
+/**
+ * Created by ${ lknny@163.com } on 2018/4/20.
+ */
+public class 二分查找 {
+	public static int[]  array={1,2,3,4,5,6,7,8,9,10};
+
+	/*递归算法*/
+	public static int binSearch(int[] array,int begin,int end,int key){
+		int index = (begin+end) / 2;
+		if (begin > end) {
+			return -1;
+		}
+		if (array[index] == key) {
+			return index;
+		}
+		if (array[index] > key) {
+			return binSearch(array, begin, index - 1, key);
+		} else {
+			return binSearch(array, index + 1, end, key);
+		}
+	}
+
+	public static int binSearch2(int[]array,int begin,int end,int key) {
+
+		while (begin <= end) {
+			int index = (begin + end) / 2;
+			if (array[index] == key) {
+				return index;
+			}
+			if (array[index] > key) {
+				end = index;
+			} else {
+				begin = index;
+			}
+		}
+		return -1;
+	}
+
+
+
+
+	public static void main(String[] args) {
+		System.out.println(binSearch(array,0,10,7));
+		System.out.println(binSearch2(array,0,10,7));
+	}
+}
